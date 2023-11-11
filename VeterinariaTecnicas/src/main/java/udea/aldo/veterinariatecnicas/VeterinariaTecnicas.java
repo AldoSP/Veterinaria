7cf6b7c83f70c76bfa0fcf449bdc616e701b9a3e
Juan Camilo Arboleda <80018262+JuanCamiloArboleda@users.noreply.github.com>
2023-11-10 23:14:53 -0500

    Add files via upload
    Se agrega base de datos y codigo adaptado para hacer posible su ejecucion

--- VeterinariaTecnicas/src/main/java/udea/aldo/veterinariatecnicas/VeterinariaTecnicas.java
+++ VeterinariaTecnicas/src/main/java/udea/aldo/veterinariatecnicas/VeterinariaTecnicas.java
@@ -12,5 +12,7 @@ public class VeterinariaTecnicas {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        CConection objetoConexion = new CConection();
+        objetoConexion.estableceConexion();
     }
 }
