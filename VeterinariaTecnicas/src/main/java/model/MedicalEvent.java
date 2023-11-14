e18852145295de180a20759d09c7a863b4150867
AldoSP <99729660+AldoSP@users.noreply.github.com>
2023-11-13 22:03:23 -0500

    Latest Change

--- VeterinariaTecnicas/src/main/java/model/MedicalEvent.java
+++ VeterinariaTecnicas/src/main/java/model/MedicalEvent.java
@@ -14,5 +14,117 @@ public class MedicalEvent {
     private Date date; //The date in which the MedicalEvent took place
     private String description; //Description of what happened
     private Vet vet; //The vet that helped the pet.
+<<<<<<< Updated upstream
     // Other attributes and methods
 }
+=======
+    private int consultationCost, extraExpenses, paidAmount;
+    private ArrayList<Medication> medicationList = new ArrayList<>();
+
+    public MedicalEvent(Date date, String description, Vet vet) {
+        this.date = date;
+        this.description = description;
+        this.vet = vet;
+    }
+
+    public MedicalEvent(int eventID, Pet pet, String description, String notes, int consultationCost, int extraExpenses, int paidAmount) {
+        this.eventID = eventID;
+        this.pet = pet;
+        this.description = description;
+        this.notes = notes;
+        this.consultationCost = consultationCost;
+        this.extraExpenses = extraExpenses;
+        this.paidAmount = paidAmount;
+    }
+
+    public void addMedication(Medication medication) {
+        medicationList.add(medication);
+    }
+
+    public void removeMedication(Medication medication) {
+        medicationList.remove(medication);
+    }
+
+    //Getters & Setters
+    public ArrayList<Medication> getMedicationList() {
+        return medicationList;
+    }
+
+    public void setMedicationList(ArrayList<Medication> medicationList) {
+        this.medicationList = medicationList;
+    }
+
+    public int getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(int eventID) {
+        this.eventID = eventID;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Vet getVet() {
+        return vet;
+    }
+
+    public void setVet(Vet vet) {
+        this.vet = vet;
+    }
+
+    public int getConsultationCost() {
+        return consultationCost;
+    }
+
+    public void setConsultationCost(int consultationCost) {
+        this.consultationCost = consultationCost;
+    }
+
+    public int getExtraExpenses() {
+        return extraExpenses;
+    }
+
+    public void setExtraExpenses(int extraExpenses) {
+        this.extraExpenses = extraExpenses;
+    }
+
+    public int getPaidAmount() {
+        return paidAmount;
+    }
+
+    public void setPaidAmount(int paidAmount) {
+        this.paidAmount = paidAmount;
+    }
+    
+}
+>>>>>>> Stashed changes
